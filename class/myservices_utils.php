76ab1c7ce9775df615c75bba04d8edced9cd2698
mambax7 <mambax7@gmail.com>
2018-04-07 19:04:04 -0400

    updates
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- class/myservices_utils.php
+++ class/myservices_utils.php
@@ -226,7 +226,7 @@ class myservices_utils
     public static function IP()
     {
         $proxy_ip = '';
-        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+       if (\Xmf\Request::hasVar('HTTP_X_FORWARDED_FOR', 'SERVER')) {
             $proxy_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) {
             $proxy_ip = $_SERVER['HTTP_X_FORWARDED'];
